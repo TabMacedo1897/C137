55e42177fb5a2a0b0f749c1b1b01a6afb26255c4
m1gu3l022 <tabata.macedo1897@gmail.com>
2023-11-29 10:36:56 -0300

    1 commit aula 138

--- train_bot.py
+++ train_bot.py
@@ -1,14 +1,78 @@
-#Biblioteca de pré-processamento de dados de texto
+# Bibliotecas necessárias
+import nltk
+from nltk.stem import PorterStemmer
+import json
+import pickle
+import numpy as np
 
+# python -m nltk.downloader punkt
 
-#função para anexar palavras-tronco
+# Instância de um stemmer da NLTK para extrair o radical das palavras
+stemmer = PorterStemmer()
 
+# Inicialização de variáveis
+words = []             # Lista para armazenar todas as palavras no arquivo de treinamento
+classes = []           # Lista para armazenar todas as tags (categorias)
+word_tags_list = []    # Lista para armazenar pares (palavras, tag) de cada padrão
+ignore_words = ['?', '!', ',', '.', "'s", "'m"]  # Palavras a serem ignoradas durante o pré-processamento
+train_data_file = open('intents.json').read()   # Carrega o arquivo JSON contendo padrões de treinamento
+intents = json.loads(train_data_file)           # Analisa o arquivo JSON
 
+def get_stem_words(words, ignore_words):
+    stem_words = []
+    for word in words:
+        if word not in ignore_words:
+            w = stemmer.stem(word.lower())
+            stem_words.append(w)
+    return stem_words
 
+#For para fazer a leitura dos dados do meu JSON
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        pattern_words: nltk.word_tokenize(pattern)
+        words.extend(pattern_words)
+        word_tags_list.append(pattern_words, intent['tag'])
+    #Adiciona as tags a lista de classes
+    if intent['tag'] not in classes:
+        classes.append(intent['tag'])
+
+stem_words = get_stem_words(words, ignore_words)
+
+print(stem_words)
+print(word_tags_list[0])
+print(classes)
+
+def create_bot_corpos(stem_words, classes):
+    stem_words = sorted(list(set(stem_words)))
+    classes = sorted(list(set(classes)))
+    pickle.dump(stem_words, open('words.pkl', 'wb'))
+    pickle.dump(classes, open('classes.pkl', 'wb'))
+    return stem_words, classes
+
+stem_words, classes = create_bot_corpos(stem_words, classes)
+print(stem_words)
+print(classes)
+
+training_data = []
+number_of_tags = len(classes)
+labels = [0] * number_of_tags
+
+#Criação dos sacos de palavras
+for word_tags in word_tags_list:
+    bag_of_words = []
+    pattern_words = word_tags[0]
+
+    for word in pattern_words:
+        index = pattern_words.index(word)
+        pattern_words[index] = word
     
-        # Adicione todas as palavras dos padrões à lista
-       
-        # Adicione todas as tags à lista classes
+    #Cria o saco de palavras
+    for word in stem_words:
+        if word in pattern_words:
+            bag_of_words.append(1)
+        else: 
+           bag_of_words.append(0) 
 
 
-#Crie o corpus de palavras para o chatbot
+
+
