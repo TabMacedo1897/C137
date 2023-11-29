a81d9c2bd07eec7bc3f0fec6eed12b85037f9436
m1gu3l022 <tabata.macedo1897@gmail.com>
2023-11-29 11:22:36 -0300

    1 commit

--- train_bot.py
+++ train_bot.py
@@ -29,9 +29,9 @@ def get_stem_words(words, ignore_words):
 #For para fazer a leitura dos dados do meu JSON
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-        pattern_words: nltk.word_tokenize(pattern)
+        pattern_words = nltk.word_tokenize(pattern)
         words.extend(pattern_words)
-        word_tags_list.append(pattern_words, intent['tag'])
+        word_tags_list.append((pattern_words, intent['tag']))
     #Adiciona as tags a lista de classes
     if intent['tag'] not in classes:
         classes.append(intent['tag'])
@@ -42,14 +42,14 @@ print(stem_words)
 print(word_tags_list[0])
 print(classes)
 
-def create_bot_corpos(stem_words, classes):
+def create_bot_corpus(stem_words, classes):
     stem_words = sorted(list(set(stem_words)))
     classes = sorted(list(set(classes)))
     pickle.dump(stem_words, open('words.pkl', 'wb'))
     pickle.dump(classes, open('classes.pkl', 'wb'))
     return stem_words, classes
 
-stem_words, classes = create_bot_corpos(stem_words, classes)
+stem_words, classes = create_bot_corpus(stem_words, classes)
 print(stem_words)
 print(classes)
 
@@ -62,8 +62,7 @@ for word_tags in word_tags_list:
     bag_of_words = []
     pattern_words = word_tags[0]
 
-    for word in pattern_words:
-        index = pattern_words.index(word)
+    for index, word in enumerate(pattern_words):
         pattern_words[index] = word
     
     #Cria o saco de palavras
@@ -72,7 +71,23 @@ for word_tags in word_tags_list:
             bag_of_words.append(1)
         else: 
            bag_of_words.append(0) 
+    print(bag_of_words)
 
+    labels_encoding = list(labels)
+    tag = word_tags[1]
+    tag_index = classes.index(tag)
+    labels_encoding[tag_index] = 1
+    training_data.append([bag_of_words, labels_encoding])
+print(training_data[0])
 
+#Cria os dados de treinamento
+def preprocess_train_data(training_data):
+    training_data = np.array(training_data, dtype=object)
+    train_x = list(training_data[:, 0])
+    train_y = list(training_data[:, 1])
 
+    print(train_x[0])
+    print(train_y[0])
+    return train_x, train_y
 
+train_x, train_y = preprocess_train_data(training_data)
